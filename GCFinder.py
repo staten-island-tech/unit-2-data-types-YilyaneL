4bfa01b9828a99da9b67377413e0ff8910ca8e34
BathTubTheGreat <yilyanel@nycstudents.net>
2026-02-06 10:17:00 -0500

    did som estuffs

--- GCFinder.py
+++ GCFinder.py
@@ -1,6 +1,16 @@
-num1 = input("put your first number ")
-num2 = input("put your second number")
+num1 = int(input("put your first number "))
+num2 = int(input("put your second number "))
 def gcf_finder():
-    
-    
+    x=1
+    y=1
+    for i in (range(x, num1)):
+        if (num1 % x == 0):
+            num1f = x
+    for i in (range(y, num2)):
+        if (num2 % y == 0):
+            num2f = y
+    x = x + 1
+    y = y + 1
+    if num2f == num1f:
+        print(max(num2f))
 gcf_finder()
--- factorfinder.py
+++ factorfinder.py
@@ -2,8 +2,7 @@ def find_factor():
     x = 1
     num = int(input("number you want to find the factors of: "))
     for i in (range(x, num)):
-        x = x + 1
         if (num % x == 0):
             print(x)
-
+    x = x + 1
 find_factor()
