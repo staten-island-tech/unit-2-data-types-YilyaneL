bec59d8100bc22e960aebd9d99b2b7301b9780a5
BathTubTheGreat <yilyanel@nycstudents.net>
2026-02-12 09:56:37 -0500

    FINSIHED GCF FIDNER GOAT

--- GCFinder.py
+++ GCFinder.py
@@ -1,10 +1,8 @@
-num1 = int(input("put your first number (smaller) "))
-num2 = int(input("put your second number (bigger) "))
+num1 = int(input("put your first number "))
+num2 = int(input("put your second number "))
 def gcf_finder():
     gcf = 0
-    if num1 > num2:
-            print("please make sure your first number is smaller than the second... or else")
-    for i in range(2, num2):
+    for i in range(2, num2 + 1):
         if (num1 % i == 0 and num2 % i == 0):
             gcf = i
     print(gcf)
